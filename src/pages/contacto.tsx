bcfed61e3380911a84d685fd6a297166bdba1ec4
holasoymolly <hola@mollyyllom.com>
2024-12-16 11:47:23 -0400

    creé la página de contacto

---
+++ src/pages/contacto.tsx
@@ -0,0 +1,13 @@
+export default function Contacto() {
+    return (
+      <div className="relative min-h-screen bg-black text-white flex flex-col items-center justify-center">
+        
+        {/* Contenido principal */}
+        <main className="text-center">
+          <h1 className="text-9xl font-extrabold mb-4">CONTACTO</h1>
+          <p className="text-lg">estoesmarialunares@gmail.com</p>
+        </main>
+
+      </div>
+    );
+  }
